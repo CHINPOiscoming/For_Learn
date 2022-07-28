5893cf5f6d66a30f5635d8c1811c06bf1ac08c22
gordon.su <gordon.su@tonic.com.tw>
2022-07-28 16:37:48 +0800

    Gordon Commit 輸入帳號密碼框架

---
+++ app/src/main/java/com/gordon/forlearn/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.gordon.forlearn
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity :AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
